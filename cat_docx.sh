0742adeaefc82546945663f665571852200a0b9e
dunganau <36718949+dunganau@users.noreply.github.com>
2021-07-14 16:39:49 -0600

    Update cat_docx.sh

--- cat_docx.sh
+++ cat_docx.sh
@@ -1,2 +1,5 @@
- mkdir edit
+#concatinate all docx files individually with "delete.docx" (or any other file)
+#delete.docx file contains all text that is desired to be deleted within a docment, using a macro that searches for duplicate paragraphs then deletes both.
+#another useful thing to do would be to insert a rubric document for grading
+mkdir edit
  for i in *.docx; do pandoc delete.docx "$i" -o edit/"$i";done
